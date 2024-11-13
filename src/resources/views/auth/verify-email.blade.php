cf80297d42de384dfa5a6badbc864e6bb666a985
pagu <ryo0417ryo@icloud.com>
2024-11-13 14:05:34 +0900

    attendance-form

--- src/resources/views/auth/verify-email.blade.php
+++ src/resources/views/auth/verify-email.blade.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Email Verification</title>
 </head>
+
 <body>
     <div class="content">
         <h1>メール認証</h1>
-        <p>先に進む前に、認証リンクのEメールを確認してください。</p>
+        <p>認証リンクのEメールを確認してください。</p>
         <p>メールが届かない場合は、下記を確認メールをおして再送信をして下さい。</p>
         @if (session('status') == 'verification-link-sent')
         <div class="alert alert-success" role="alert">
@@ -21,4 +23,5 @@
         </form>
     </div>
 </body>
+
 </html>
